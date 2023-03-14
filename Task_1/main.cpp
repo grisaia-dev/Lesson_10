7c4993edc86256a4bd399b2fc4ef94446ecce51a
Grisaia <grisaia@inbox.ru>
2023-03-14 16:29:06 +0900

    Task_2

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-
-#define WINDOWS
-#ifndef WINDOWS
 #include <Windows.h>
-#endif
 
 int main(void) {
-
-#ifndef WINDOWS
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-#endif
 
     std::string name = "";
     std::cout << "Введите имя: ";
